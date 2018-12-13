2bac71dae771f330c01275e81a1c17cfb8f33ba3
saroopchand <saroop.chand@progos.org>
2018-12-13 16:48:50 +0500

    Used basic jQuery to update cart weight tab to on click of addtocart and update button

--- view/frontend/web/js/cartweight.js
+++ view/frontend/web/js/cartweight.js
@@ -5,23 +5,46 @@ define([
     "use strict";
 
     function main(config, element) {
+        $(document).ready(function(){
+            callAjax(config, element);
+            $('#product-addtocart-button').on('click',function(){
+                addUpdateAction( config, element )
+            });
+
+            $('.update-cart-item').on('click',function(){
+                addUpdateAction( config, element )
+            });
+
+        });
+    };
+
+    function addUpdateAction( config, element ){
+        $(document).ajaxComplete(function (event, xhr, settings) {
+            if (settings.url.indexOf("customer/section/load/?sections=cart") > 0) {
+                var cartObj = xhr.responseJSON;
+                if( cartObj.cart.summary_count > 0 ){
+                    callAjax(config, element);
+                }
+            }
+        });
+    }
+
+    function callAjax( config, element ){
         var $element = $(element);
         var AjaxUrl = config.AjaxUrl;
         var weightUnit = config.weightUnit;
         var currentProductWeight = config.currentProductWeight;
 
-        $(document).ready(function(){
-            $.ajax({
-                context: '#ajaxresponse',
-                url: AjaxUrl,
-                type: "post",
-                data: {},
-            }).done(function (data) {
-                var afterproductaddtocart = parseFloat(currentProductWeight) + parseFloat(data.cartweight);
-                $('#total-cart-weight').html("Total Cart Weight: "+data.cartweight+" "+weightUnit);
-                $('#afterproductaddtocart').html("Cart Weight after this product: "+afterproductaddtocart+" "+weightUnit);
-                return true;
-            });
+        $.ajax({
+            context: '#ajaxresponse',
+            url: AjaxUrl,
+            type: "post",
+            data: {},
+        }).done(function (data) {
+            var afterproductaddtocart = parseFloat(currentProductWeight) + parseFloat(data.cartweight);
+            $('#total-cart-weight').html("Total Cart Weight: "+data.cartweight+" "+weightUnit);
+            $('#afterproductaddtocart').html("Cart Weight after this product: "+afterproductaddtocart+" "+weightUnit);
+            return true;
         });
     };
     return main;
